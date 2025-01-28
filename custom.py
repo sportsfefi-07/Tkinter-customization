aabfdf250eada1c9d05eece723b6fd5026ee1545
sports-fefi07 <fd021307@wgcloud.org>
2025-01-28 11:27:57 -0600

    Added window 1

--- custom.py
+++ custom.py
@@ -6,7 +6,7 @@ window1 = tk.Tk()
 window1.title("Window 1")
 window1.geometry("400x400")
 window1.configure(bg= "#C41E3A")
-
+window1.iconbitmap("images/St_Louis_Cardinals_logo.ico")
 
 
 window1.mainloop()
