bde3908accbc2b34a13274e36b41a57a0f296c37
sports-fefi07 <fd021307@wgcloud.org>
2025-01-28 11:10:17 -0600

    fixed bullets

---
+++ custom.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+######window settings######
+window1 = tk.Tk()
+window1.title("Window 1")
+window1.geometry("400x400")
+window1.configure(bg= "#C41E3A")
+
+
+
+window1.mainloop()
